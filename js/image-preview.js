ab8b349e831593b7efdfb0edd33fdb46362c5888
hojinh <hoinh@gmail.com>
2022-02-15 09:03:22 +0900

    create multi card

---
+++ js/image-preview.js
@@ -0,0 +1,102 @@
+class ImagePreview extends ComponentABS{
+  constructor()
+  {
+      super();    
+  }
+  static get observedAttributes() {return ['any_attribute']; }
+
+  handleClick(e) {
+      e.composedPath().find((node) => 
+      {
+          if (!node.className || !node.className?.match(/command/)) return false;
+          if (node.className.match(/command-find-file/))
+            {
+              this.shadowRoot.querySelector('input#upload_image').click();
+              return ;
+            }
+      });
+  }
+  onMessage(event){
+      const window_url = `https://${window.location.hostname}`;
+      //if(event.origin !== window_url) return;
+      if(event.data?.msg) 
+      {
+          // if(event.data.msg === `${this.message_prefix}_show_user_name_card`) 
+          // {
+          //     this._render(event.data.data);
+          // }
+      }
+  }
+
+  connectedCallback() {
+      this._render();
+  }
+      
+  disconnectedCallback(){
+      console.log("disconnectedCallback");
+      window.removeEventListener("message", this.receiveMessage);
+  }
+  
+  attributeChangedCallback(name, oldValue, newValue) {
+      console.log('image Preview element attributes changed.'); 
+      if(name == 'placeholder') this._setInfoText(newValue);
+  }
+
+  _render()
+  {
+      const template = document.querySelector('template#image_preview');
+      const shadowRoot = this.attachShadow({mode: 'open'});
+      shadowRoot.appendChild(template.content.cloneNode(true));
+      
+      const infoText = this.getAttribute('placeholder');
+      this._setInfoText(infoText);
+      
+      this.shadowRoot.querySelector('input#upload_image').addEventListener('change', e => this._load_image(e));
+  }
+  
+  _load_image = e => {
+    const files = e.target.files;
+    const filesArr = Array.prototype.slice.call(files);
+
+    // 여러장의 이미지를 불러올 경우, 배열화
+    filesArr.forEach(file => {
+        const reader = new FileReader();
+        reader.onload = e => {
+            const image = new Image();
+            image.className = "img-item"; // 스타일 적용을 위해
+            image.src = e.target.result;
+            image.onload = imageEvent => {
+              this.shadowRoot.querySelector('.add-image-zone').style.border = 'none'
+              this.shadowRoot.querySelector('.add-image-zone').style.backgroundImage = `url(${e.target.result})`;
+              this.shadowRoot.querySelector('.add-image-zone').style.borderRadius = 'unset';
+              this.shadowRoot.querySelector('.image-upload-info > p').style.display = 'none';
+              this._after_preview();
+            };
+            image.onerror = function() {
+                console.log("error");
+            };
+        };
+        reader.readAsDataURL(file);
+    });
+  };
+
+  _setInfoText(newValue)
+  {
+    this.shadowRoot.querySelector('.image-upload-info > p').innerText = newValue;
+  }
+  _after_preview()
+  {
+    //..do something after preview
+    // form action, save image ..
+  }
+
+}
+customElements.define('image-preview', ImagePreview);
+
+
+
+
+
+ 
+
+
---
+++ js/media-article.js
@@ -0,0 +1,79 @@
+class MedaiArticle extends ComponentABS{
+  constructor(data = null)
+  {
+      super();
+
+      if(data)
+      this.data = data;
+      else this.data =  this._get_initial_data();
+  }
+
+  static get observedAttributes() {return ['any_attribute']; }
+
+  handleClick(e) {
+      e.composedPath().find((node) => 
+      {
+          if (!node.className || !node.className.match(/command/)) return false;
+      });
+  }
+  onMessage(event){
+      const window_url = `https://${window.location.hostname}`;
+      //if(event.origin !== window_url) return;
+      if(event.data?.msg) 
+      {
+          // if(event.data.msg === `${this.message_prefix}_show_user_name_card`) 
+          // {
+          //     this._render(event.data.data);
+          // }
+      }
+  }
+
+  connectedCallback() {
+      this._render(this.data);
+  }
+      
+  disconnectedCallback(){
+      console.log("disconnectedCallback");
+      window.removeEventListener("message", this.receiveMessage);
+  }
+  
+  attributeChangedCallback(name, oldValue, newValue) {
+      console.log('Media Card element attributes changed.'); 
+  }
+
+  _render()
+  {
+    const template = document.querySelector('template#media_article');
+    const shadowRoot = this.attachShadow({mode: 'open'});
+    shadowRoot.appendChild(template.content.cloneNode(true));
+    setTimeout(() => {
+        this._hide_animation();
+    }, this.data.duration);
+  }
+
+  _get_initial_data()
+  {
+     const temp_data = {};
+     temp_data.create_timestamp =  new Date().getTime();
+     temp_data.end_timestatmp = temp_data.create_timestamp + (Math.floor(Math.random() * 15) +1) *1000;
+     temp_data.duration = (temp_data.end_timestatmp - temp_data.create_timestamp);
+     return temp_data;
+  }
+
+  _hide_animation()
+  {
+    this.shadowRoot.querySelector('article').classList.add('disappear');
+    setTimeout(() => {
+        this.remove();
+    }, 1000);
+  }
+}
+customElements.define('media-article', MedaiArticle);
+
+
+
+
+
+ 
+
+
